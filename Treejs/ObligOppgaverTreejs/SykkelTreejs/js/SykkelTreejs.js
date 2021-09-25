35e35eb430c948c5fce72fed01cc77993c28f65e
Halil Ibrahim Keser <hke006@uit.no>
2021-09-25 15:52:31 +0200

    sykkel

--- Treejs/ObligOppgaverTreejs/SykkelTreejs/js/SykkelTreejs.js
+++ Treejs/ObligOppgaverTreejs/SykkelTreejs/js/SykkelTreejs.js
@@ -1,9 +1,10 @@
-import * as THREE from '../../lib/three/build/three.module.js';
-import {TrackballControls} from '../../lib/three/examples/jsm/controls/TrackballControls.js';
-import {addCoordSystem} from "../../lib/wfa-coord.js";
+import * as THREE from '../../../lib/three/build/three.module.js';
+import {TrackballControls} from '../../../lib/three/examples/jsm/controls/TrackballControls.js';
+import {addCoordSystem} from "../../../lib/wfa-coord.js";
 
 let renderer;
 let scene;
+
 let camera;
 let angle = 0.0;
 let lastTime = 0.0;
@@ -16,8 +17,8 @@ let controls; //rotere, zoome hele scenen.
 
 let SIZE = 200;
 
-let helicopter;
-let heliSpeed = 0.0;
+let bicycle;
+let heliSpeed = 0.0; //TODO
 let speedVector = new THREE.Vector3(0.3,0,0.1);
 let positionVector = new THREE.Vector3(0.3,0,0.1);
 let delta = Math.PI/100;  						//Hvor mye fartsvektoren roterer
@@ -39,9 +40,9 @@ export function main() {
 
 	//KAMERA
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-	camera.position.x = 130;
-	camera.position.y = 200;
-	camera.position.z = 130;
+	camera.position.x = 80;
+	camera.position.y = 50;
+	camera.position.z = 70;
 	camera.up = new THREE.Vector3(0, 1, 0);
 
 	lookAtX = 0.0;
@@ -107,8 +108,11 @@ function addPlane() {
 }
 
 function addBicycle() {
+	bicycle = new THREE.Object3D();
+
 	let texturesToLoad = [
 		{name: 'rammeTexture', url: '../images/black-metal.jpg'},
+		{name: 'wheelTexture', url: '../images/rubber_texture1332.jpg'}
 	];
 
 	let loadedTexures={};
@@ -122,154 +126,144 @@ function addBicycle() {
 				texturesToLoad.splice( texturesToLoad.indexOf(image), 1);
 
 				if ( !texturesToLoad.length ) {
-					let cube = new THREE.Group();
-					cube.position.x = 20;
-					cube.position.y = 0;
-					cube.position.z = 0;
-					cube.scale.x = 10;
-					cube.scale.y = 10;
-					cube.scale.z = 10;
+					let frame = new THREE.Group();
+					frame.position.x = 0;
+					frame.position.y = 50;
+					frame.position.z = 0;
+					frame.scale.x = 20;
+					frame.scale.y = 20;
+					frame.scale.z = 20;
 
-					// CUBE
-					let materialCube = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
-					let geometryCube = new THREE.BoxGeometry(2,2,2)
-					let cubeMesh = new THREE.Mesh( geometryCube, materialCube );
-					cubeMesh.castShadow = true;
-					cubeMesh.name = "Cube";
-					cubeMesh.position.x = 0;
-					cubeMesh.position.y = 1;
-					cubeMesh.position.z = 6;
-					cube.add(cubeMesh);
+					// FRAME
+					let materialFrameTop = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameTop = new THREE.CylinderGeometry(0.06, 0.06, 3, 30, 1, false, 0, 2*Math.PI)
+					let frameMeshTop = new THREE.Mesh( geometryFrameTop, materialFrameTop );
+					frameMeshTop.castShadow = true;
+					// ITORS - I * T (position) * O (orbit) * R (rotation) * S (scale)  der O = R * T
+					frameMeshTop.name = "RammeTop";
+					frameMeshTop.position.x = 0;
+					frameMeshTop.position.y = 0;
+					frameMeshTop.position.z = 0;
+					frameMeshTop.rotation.x = 1.75  //<--NB radianer
+					frame.add(frameMeshTop);
 
-					scene.add(cube)
+					let materialFrameBack = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameBack = new THREE.CylinderGeometry(0.08, 0.08, 2, 30, 1, false, 0, 2*Math.PI)
+					let frameMeshBack = new THREE.Mesh( geometryFrameBack, materialFrameBack );
+					frameMeshBack.castShadow = true;
+					frameMeshBack.name = "RammeBak";
+					frameMeshBack.position.x = 0;
+					frameMeshBack.position.y = -0.7;
+					frameMeshBack.position.z = 1.4;
+					frameMeshBack.rotation.x = 0.2
+					frame.add(frameMeshBack);
 
-					animate();
-				}
-				console.log('[TextureLoader] Loaded %o', image.name);
-			},
-			undefined,
-			function ( err ) {
-				console.error( 'Feil ved lasting av teksturfil...' );
-			});
-	}
-}
+					let materialFrameBackTopRight = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameBackTopRight = new THREE.CylinderGeometry(0.03, 0.03, 1.7, 30, 1, false, 0, 2*Math.PI)
+					let frameMeshBackTopRight = new THREE.Mesh( geometryFrameBackTopRight, materialFrameBackTopRight );
+					frameMeshBackTopRight.castShadow = true;
+					frameMeshBackTopRight.name = "RammeBak";
+					frameMeshBackTopRight.position.x = 0.08;
+					frameMeshBackTopRight.position.y = -0.85;
+					frameMeshBackTopRight.position.z = 2.1;
 
-function addCup() {
+					frameMeshBackTopRight.rotation.x = -0.8
+					frame.add(frameMeshBackTopRight);
 
-	let texturesToLoad = [
-		{name: 'cubTexture', url: 'images/Tileable_Mosaic.jpg'},
-		{name: 'coffeeTexture', url: 'images/chocchip.png'}
-	];
+					let materialFrameBackDownRight = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameBackDownRight = new THREE.CylinderGeometry(0.04, 0.04, 1.55, 30, 2, false, 0, 2*Math.PI)
+					let frameMeshBackDownRight = new THREE.Mesh( geometryFrameBackDownRight, materialFrameBackDownRight );
+					frameMeshBackDownRight.castShadow = true;
+					frameMeshBackDownRight.name = "RammeBak";
+					frameMeshBackDownRight.position.x = 0.08;
+					frameMeshBackDownRight.position.y = -1.53;
+					frameMeshBackDownRight.position.z = 1.95;
+					frameMeshBackDownRight.rotation.x = 1.45
+					frame.add(frameMeshBackDownRight);
 
-	let loadedTexures={};
-	const loader = new THREE.TextureLoader();
+					let materialFrameBackTopLeft = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameBackTopLeft = new THREE.CylinderGeometry(0.03, 0.03, 1.7, 30, 1, false, 0, 2*Math.PI)
+					let frameMeshBackTopLeft = new THREE.Mesh( geometryFrameBackTopLeft, materialFrameBackTopLeft );
+					frameMeshBackTopLeft.castShadow = true;
+					frameMeshBackTopLeft.name = "RammeBak";
+					frameMeshBackTopLeft.position.x = -0.08;
+					frameMeshBackTopLeft.position.y = -0.85;
+					frameMeshBackTopLeft.position.z = 2.1;
 
-	for ( let image of texturesToLoad ) {
-		loader.load(
-			image.url,
-			( texture ) => {
-				loadedTexures[image.name] = texture;
-				texturesToLoad.splice( texturesToLoad.indexOf(image), 1);
+					frameMeshBackTopLeft.rotation.x = -0.8
+					frame.add(frameMeshBackTopLeft);
 
-				if ( !texturesToLoad.length ) {
-					let cup = new THREE.Group();
-					cup.position.x = 20;
-					cup.position.y = 0;
-					cup.position.z = 0;
-					cup.scale.x = 10;
-					cup.scale.y = 10;
-					cup.scale.z = 10;
+					let materialFrameBackDownLeft = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameBackDownLeft = new THREE.CylinderGeometry(0.04, 0.04, 1.55, 30, 2, false, 0, 2*Math.PI)
+					let frameMeshBackDownLeft = new THREE.Mesh( geometryFrameBackDownLeft, materialFrameBackDownLeft );
+					frameMeshBackDownLeft.castShadow = true;
+					frameMeshBackDownLeft.name = "RammeBak";
+					frameMeshBackDownLeft.position.x = -0.08;
+					frameMeshBackDownLeft.position.y = -1.53;
+					frameMeshBackDownLeft.position.z = 1.95;
+					frameMeshBackDownLeft.rotation.x = 1.45
+					frame.add(frameMeshBackDownLeft);
 
-					let materialCup = new THREE.MeshPhongMaterial({map : loadedTexures['cubTexture'], side: THREE.DoubleSide});	//NB! MeshPhongMaterial
+					let materialFrameFront = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameFront = new THREE.CylinderGeometry(0.07, 0.07, 3.2, 30, 1, false, 0, 2*Math.PI)
+					let frameMeshFront = new THREE.Mesh( geometryFrameFront, materialFrameFront );
+					frameMeshFront.castShadow = true;
+					frameMeshFront.name = "RammeFront";
+					frameMeshFront.position.x = 0;
+					frameMeshFront.position.y = -0.912;
+					frameMeshFront.position.z = -0.2;
+					frameMeshFront.rotation.x = -1.1
+					frame.add(frameMeshFront);
 
-					// Bunnen
-					let geometryCylinder = new THREE.CylinderGeometry( 0.4, 0.4, 0.05, 32 );
-					let bottomMesh = new THREE.Mesh( geometryCylinder, materialCup );
+					let materialFrameFrontSmall = new THREE.MeshPhongMaterial({map : loadedTexures['rammeTexture'], side: THREE.DoubleSide});
+					let geometryFrameFrontSmall = new THREE.CylinderGeometry(0.07, 0.07, 0.56, 30, 1, false, 0, 2*Math.PI)
+					let frameMeshFrontSmall = new THREE.Mesh( geometryFrameFrontSmall, materialFrameFrontSmall );
+					frameMeshFrontSmall.castShadow = true;
+					frameMeshFrontSmall.name = "RammeFront";
+					frameMeshFrontSmall.position.x = 0;
+					frameMeshFrontSmall.position.y = 0;
+					frameMeshFrontSmall.position.z = -1.5;
+					frameMeshFrontSmall.rotation.x = 0.30
+					frame.add(frameMeshFrontSmall);
 
-					//Koppen/Lathe:
-					let points = [];
-					for (let x = 0; x < 1; x=x+0.01) {
-						let y = Math.pow(x,5)*2;
-						points.push(new THREE.Vector2(x,y));
-					}
-					let geometryCup = new THREE.LatheGeometry(points, 128, 0, 2 * Math.PI);
-					let meshCup = new THREE.Mesh(geometryCup, materialCup);
-					bottomMesh.add(meshCup);
+					// BACK WHEEL
+					let materialWheelBack = new THREE.MeshPhongMaterial({map : loadedTexures['wheelTexture'], side: THREE.DoubleSide});
+					let geometryWheelBack = new THREE.TorusGeometry(1, 0.07, 50, 100, Math.PI * 2)
+					let wheelBackMesh = new THREE.Mesh( geometryWheelBack, materialWheelBack );
+					wheelBackMesh.castShadow = true;
+					wheelBackMesh.name = "RammeFront";
+					wheelBackMesh.position.x = 0;
+					wheelBackMesh.position.y = -1.4;
+					wheelBackMesh.position.z = 2.5;
+					wheelBackMesh.rotation.y = 1.57
+					frame.add(wheelBackMesh);
 
-					// Kaffen
-					let geometryCoffee = new THREE.CircleGeometry( 0.9, 32 );
-					let materialCoffee = new THREE.MeshPhongMaterial({color:0x7F4600, map : loadedTexures['coffeeTexture']});	//NB! MeshPhongMaterial
-					let coffeeMesh = new THREE.Mesh( geometryCoffee, materialCoffee );
-					coffeeMesh.rotation.x = -Math.PI/2;
-					coffeeMesh.position.y = 1.4;
-					bottomMesh.add( coffeeMesh );
 
-					// Hanken/torus
-					let geometryTorus = new THREE.TorusGeometry( 15, 3, 16, 100, Math.PI );
-					let meshTorus = new THREE.Mesh( geometryTorus, materialCup );
-					meshTorus.rotation.z = -Math.PI/2 - Math.PI/14;
-					meshTorus.scale.x=0.035;
-					meshTorus.scale.y=0.035;
-					meshTorus.scale.z=0.035;
-					meshTorus.position.x = 0.8;
-					meshTorus.position.y = 1;
-					bottomMesh.add( meshTorus );
+					// STEERING
+					let steering = new THREE.Group();
+					steering.position.x = 0;
+					steering.position.y = 0;
+					steering.position.z = 0;
+					steering.scale.x = 1;
+					steering.scale.y = 1;
+					steering.scale.z = 1;
 
-					cup.add( bottomMesh );
-					scene.add(cup);
+					// FRONT WHEEL
+					let materialWheelFront = new THREE.MeshPhongMaterial({map : loadedTexures['wheelTexture'], side: THREE.DoubleSide});
+					let geometryWheelFront = new THREE.TorusGeometry(1, 0.07, 50, 100, Math.PI * 2)
+					let wheelFrontMesh = new THREE.Mesh( geometryWheelFront, materialWheelFront );
+					wheelFrontMesh.castShadow = true;
+					wheelFrontMesh.name = "RammeFront";
+					wheelFrontMesh.position.x = 0;
+					wheelFrontMesh.position.y = -1.4;
+					wheelFrontMesh.position.z = -2;
+					wheelFrontMesh.rotation.y = 1.57
+					steering.add(wheelFrontMesh);
 
-					// //Cockpit:
-					// let gCockpit = new THREE.SphereGeometry(5, 32, 32);
-					// let mCockpit = new THREE.MeshPhongMaterial({ map: loadedTexures['imageCockpit']  });
-					// let meshCockpit = new THREE.Mesh(gCockpit, mCockpit);
-					// meshCockpit.castShadow = true;
-					// meshCockpit.name = "cockpit";
-					// meshCockpit.position.x = 0;
-					// meshCockpit.position.y = 0;
-					// meshCockpit.position.z = 0;
-					// helicopter.add(meshCockpit);
-					//
-					// //Body:
-					// let gBody = new THREE.CylinderGeometry(1.0, 4, 12, 8, 4, false);
-					// let mBody = new THREE.MeshPhongMaterial({ map: loadedTexures['imageBody']  });
-					// let meshBody = new THREE.Mesh(gBody, mBody);
-					// meshBody.castShadow = true;
-					// meshBody.name = "body";
-					// meshBody.rotation.z = Math.PI / 2;
-					// meshBody.position.x = -7;
-					// meshBody.position.y = 0;
-					// meshBody.position.z = 0;
-					// helicopter.add(meshBody);
-					//
-					// //Rotor:
-					// let gRotor = new THREE.BoxGeometry(0.2, 20, 1);
-					// let mRotor = new THREE.MeshBasicMaterial({ color:0x00de88});
-					// let meshRotor = new THREE.Mesh(gRotor, mRotor);
-					// meshRotor.name = "rotor";
-					// meshRotor.rotation.z = Math.PI / 2;
-					// meshRotor.rotation.y = Math.PI / 5;
-					// meshRotor.position.x = 0;
-					// meshRotor.position.y = 5;
-					// meshRotor.position.z = 0;
-					// meshRotor.castShadow = true;
-					// helicopter.add(meshRotor);
-					//
-					// //Bakrotor:
-					// let gBRotor = new THREE.BoxGeometry(5, 1, 0.2);
-					// let mBRotor = new THREE.MeshBasicMaterial({ color:0x00de88});
-					// let meshBRotor = new THREE.Mesh(gBRotor, mBRotor);
-					// meshBRotor.name = "bakrotor";
-					// meshBRotor.position.x = -13.0;
-					// meshBRotor.position.y = 1;
-					// meshBRotor.position.z = 0;
-					// helicopter.add(meshBRotor);
-					//
-					// scene.add(helicopter);
-					//
-					// //Flytter hele helikoptret:
-					// helicopter.position.y = 100;
+					bicycle.add(frame)
+					bicycle.add(steering)
+					scene.add(bicycle)
 
-					// Starter løkka!
 					animate();
 				}
 				console.log('[TextureLoader] Loaded %o', image.name);
@@ -307,33 +301,12 @@ function animate(currentTime) {
 
 	lastTime = currentTime;
 
-	// let rotationSpeed = (Math.PI); // Bestemmer rotasjonshastighet.
-	// angle = angle + (rotationSpeed * elapsed);
-	// angle %= (Math.PI * 2); // "Rull rundt" dersom angle >= 360 grader.
+	//let rotationSpeed = (Math.PI); // Bestemmer rotasjonshastighet.
+	//angle = angle + (rotationSpeed * elapsed);
+	//angle %= (Math.PI * 2); // "Rull rundt" dersom angle >= 360 grader.
 
 	// //Roterer helikoptrets rotor:
-	// let rotor = helicopter.getObjectByName("rotor", true);  //true = recursive...
-	// if (rotor !== undefined)
-	// 	rotor.rotation.y = angle;
-	// let bakrotor = helicopter.getObjectByName("bakrotor", true);  //true = recursive...
-	// if (bakrotor !== undefined)
-	// 	bakrotor.rotation.z = angle;
-
-	// // Oppdaterer posisjonsvektoren vha. fartsvektoren:
-	// positionVector.x = positionVector.x + (speedVector.x * heliSpeed);
-	// positionVector.y = 0;
-	// positionVector.z = positionVector.z + (speedVector.z * heliSpeed);
-
-	// // Bruker posisjonsvektoren til å oppdatere helikoptrets posisjon:
-	// helicopter.position.x = positionVector.x;
-	// helicopter.position.z = positionVector.z;
-	//
-	// // Roterer helikoptret i forhold til fartsvektoren:
-	// helicopter.rotation.y = getRotationAngleUsingAtan2();
-	//
-	// // Illustrerer fartsvektoren:
-	// arrowHelper.setDirection(speedVector);
-	// arrowHelper.setLength(heliSpeed*100);
+	//let rotor = bicycle.getObjectByName("steering", true);  //true = recursive...
 
 	//Sjekker input:
 	keyCheck();
@@ -348,49 +321,54 @@ function animate(currentTime) {
 function keyCheck() {
 
 	if (currentlyPressedKeys[65]) { //A
-		camera.lookAt(0,50,10);
-		// controls.rotateX(10)
+		camera.position.x = camera.position.x - 30 * (Math.PI/180)
+		controls.update();
+
+		// camera.rotation.x  += 10 * (Math.PI/180);
 		// controls.update();
 	}
-	if (currentlyPressedKeys[68]) { //S
-		//camera.position.x.rotate(2,0,1,0)
-		//camera.position.rotate(2,0,1,0)
-	}
-	if (currentlyPressedKeys[87]) { //S
-		//camera.position.x.rotate(2,0,1,0)
-		//camera.position.rotate(2,0,1,0)
-	}
-	if (currentlyPressedKeys[83]) { //D
-		//camera.position.x.rotate(2,0,1,0)
-		//camera.position.rotate(2,0,1,0)
-	}
-	//RETNING
-	if (currentlyPressedKeys[74]) { //J
-		let matrix = new THREE.Matrix4().makeRotationAxis( axis, delta );
-	   	speedVector.applyMatrix4( matrix );
-    }
-    if (currentlyPressedKeys[75]) {	//K
-    	heliSpeed-=0.01;
-		if (heliSpeed<=0)
-			heliSpeed = 0.0;
-    }
-    if (currentlyPressedKeys[73]) {	//I
-    	heliSpeed+=0.01;
-		if (heliSpeed>=10)
-			heliSpeed = 1.0;
-    }
-    if (currentlyPressedKeys[76]) {	//L
-    	let matrix = new THREE.Matrix4().makeRotationAxis( axis, -delta );
-	   	speedVector.applyMatrix4( matrix );
-    }
 
-    //HØYDE
-    if (currentlyPressedKeys[78]) { //N
-    	helicopter.position.y -= 0.3;
-    }
-    if (currentlyPressedKeys[77]) {	//M
-    	helicopter.position.y += 0.3;
-    }
+	if (currentlyPressedKeys[68]) { //D
+		camera.position.x = camera.position.x + 30 * (Math.PI/180)
+		controls.update();
+	}
+
+	if (currentlyPressedKeys[87]) { //W
+		camera.position.y = camera.position.y + 30 * (Math.PI/180)
+		controls.update();
+	}
+	if (currentlyPressedKeys[83]) { //S
+		camera.position.y = camera.position.y - 30 * (Math.PI/180)
+		controls.update();
+	}
+
+	// //RETNING
+	// if (currentlyPressedKeys[74]) { //J
+	// 	let matrix = new THREE.Matrix4().makeRotationAxis( axis, delta );
+	//    	speedVector.applyMatrix4( matrix );
+    // }
+    // if (currentlyPressedKeys[75]) {	//K
+    // 	heliSpeed-=0.01;
+	// 	if (heliSpeed<=0)
+	// 		heliSpeed = 0.0;
+    // }
+    // if (currentlyPressedKeys[73]) {	//I
+    // 	heliSpeed+=0.01;
+	// 	if (heliSpeed>=10)
+	// 		heliSpeed = 1.0;
+    // }
+    // if (currentlyPressedKeys[76]) {	//L
+    // 	let matrix = new THREE.Matrix4().makeRotationAxis( axis, -delta );
+	//    	speedVector.applyMatrix4( matrix );
+    // }
+	//
+    // //HØYDE
+    // if (currentlyPressedKeys[78]) { //N
+    // 	helicopter.position.y -= 0.3;
+    // }
+    // if (currentlyPressedKeys[77]) {	//M
+    // 	helicopter.position.y += 0.3;
+    // }
 }
 
 function render()
